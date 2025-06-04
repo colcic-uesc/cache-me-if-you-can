676f2c9aa021460b21e5b0a291eff9e63b525c16
Tainah <tayhmarques.00@gmail.com>
2025-06-04 20:25:02 -0300

    initial commit

---
+++ src/main/java/com/example/cache_me_if_you_can/CacheMeIfYouCanApplication.java
@@ -0,0 +1,13 @@
+package com.example.cache_me_if_you_can;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheMeIfYouCanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacheMeIfYouCanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cache_me_if_you_can/CacheMeIfYouCanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cache_me_if_you_can;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacheMeIfYouCanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
