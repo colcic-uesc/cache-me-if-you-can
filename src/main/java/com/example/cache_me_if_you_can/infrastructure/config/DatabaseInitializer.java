cd7276e46e6e5567aea0036bccb231d13887e40c
Tainah <tayhmarques.00@gmail.com>
2025-06-08 12:09:54 -0300

    chore: organize folders

--- src/main/java/com/example/cache_me_if_you_can/infrastructure/config/DatabaseInitializer.java
+++ src/main/java/com/example/cache_me_if_you_can/infrastructure/config/DatabaseInitializer.java
@@ -1,4 +1,4 @@
-package com.example.cache_me_if_you_can.config;
+package com.example.cache_me_if_you_can.infrastructure.config;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
